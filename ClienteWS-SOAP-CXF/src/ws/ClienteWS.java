38fc666635031c7acfaf304678e7b769ab366220
Ricardo Marcelo <ricardomarcelo@hotmail.com>
2020-03-22 06:34:56 +0000

    Guardando proyectos version1

---
+++ ClienteWS-SOAP-CXF/src/ws/ClienteWS.java
@@ -0,0 +1,30 @@
+package ws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import entidad.Cliente;
+
+@WebService(name = "ClienteWS", 
+	targetNamespace = "http://ricardomarcelo.pe/ws/ventas/")
+public class ClienteWS {
+	private List<Cliente> listaCliente = new ArrayList<Cliente>();
+	
+	@WebMethod
+	@WebResult(name = "resultado")
+	public String crear(@WebParam(name = "cliente") Cliente cliente) {
+		listaCliente.add(cliente);
+		return "Cliente registrado";
+	}
+
+	@WebMethod
+	@WebResult(name = "cliente")
+	public List<Cliente> consultarTodo() {		
+		return listaCliente;
+	}
+}
---
+++ ClienteWS-SOAP-JBossWS/src/entidad/Cliente.java
@@ -0,0 +1,33 @@
+package entidad;
+
+public class Cliente {
+	private Integer id;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}	
+}
